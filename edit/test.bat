7a9d622034588013ddc3f59ba7a4e643e8cbeb82
Srgy Surkv <sss1024@gmail.com>
2020-07-05 23:03:46 +0300

    split

--- edit/test.bat
+++
@@ -1,2 +0,0 @@
-call tsc base\zvoogpopup.ts base\zvooglang.ts base\zvoogundoredo.ts public\zvoogtest.ts public\zvoogtypes.ts base\waf.ts base\filters.ts base\sources.ts base\tilelevel.ts base\MIDIFile.ts base\zvoogapp.ts --strictNullChecks --alwaysStrict --target ES5 --outFile .\build\zvoogdaw.js
-pause
